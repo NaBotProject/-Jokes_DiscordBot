5f6c5f825e3ca57057f69235cf26b18cdcb64090
m.sepehr <83414472+sepehrmizani@users.noreply.github.com>
2022-06-07 18:55:09 +0430

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,34 @@
 
+
+import discord
+import os
+import openpyxl 
+import random
+
+
+path = "onelinefun.xlsx"
+wb_obj = openpyxl.load_workbook(path) 
+sheet_obj = wb_obj.active 
+
+
+
+
+client = discord.Client()
+TOKEN = os.getenv('TOKEN')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$jok'):
+        row_x=random.randint(1, 2900)
+        cell_obj = sheet_obj.cell(row =row_x, column = 2) 
+        await message.channel.send(cell_obj.value)
+
+
+client.run(TOKEN)
